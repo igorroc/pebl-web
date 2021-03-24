95a2c9b2a3e954a269ecc250b85ccda15184bc16
IgorRoc <igor_roc@hotmail.com.br>
2021-03-24 17:12:10 -0300

    [add]: Initial script

--- js/sternberg.js
+++ js/sternberg.js
@@ -1,1 +1,29 @@
+const alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+var tempo = []
+var escolhas = []
+var nivel = 1
+
+var alternar = document.getElementsByClassName('alternar')[0]
+var lembrar = document.getElementsByClassName('lembrar')[0]
+
+var aleatorio = alfabeto[Math.floor(Math.random() * alfabeto.length)]
+alternar.innerHTML = aleatorio
+
+function pegaTempo() {
+    var d = new Date()
+    tempo.push(d)
+}
+
+window.addEventListener('keypress', function (e) {
+    var codigo = e.code.slice(3)
+    if (codigo == 'A') {
+        if (lembrar.innerHTML.search(codigo) != -1) {
+            
+        }
+    } else if(codigo == 'L'){
+        
+    }else{
+
+    }
+});
