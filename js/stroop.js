bb12c1019724e41fe5f308c836db1d1c98460a11
IgorRoc <igor_roc@hotmail.com.br>
2021-03-24 08:13:56 -0300

    [fix]: Buttons generated dynamically

--- js/stroop.js
+++ js/stroop.js
@@ -16,8 +16,22 @@ function comecar(){
     comeco.textContent = nomes[i]
     comeco.style.color = cores[i]
     document.body.style.background = "#f3f3f3 url('')"
-    
+    var div_botoes = document.getElementsByClassName('botoes')[0]
+    var botao_err = document.createElement('div')
+        botao_err.setAttribute('class', 'err')
+        botao_err.setAttribute('onclick', 'clickErrado()')
+    var p_err = document.createElement('p')
+        p_err.innerText = "Errado"
+    var botao_cer = document.createElement('div')
+        botao_cer.setAttribute('class', 'cer')
+        botao_cer.setAttribute('onclick', 'clickCerto()')
+    var p_cer = document.createElement('p')
+        p_cer.innerText = "Certo"
 
+    botao_err.appendChild(p_err)
+    botao_cer.appendChild(p_cer)
+    div_botoes.appendChild(botao_err)
+    div_botoes.appendChild(botao_cer)
     pegaTempo()
 
     elemento_pai.removeAttribute("onclick");
