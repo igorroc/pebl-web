8467206f75edcee91f14b3aaa590a572238a8946
IgorRoc <igor_roc@hotmail.com.br>
2021-03-24 18:03:10 -0300

    [add]: Fully working

--- js/sternberg.js
+++ js/sternberg.js
@@ -1,4 +1,8 @@
 const alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const PROX_NIVEL = 10
+const FIM = 30
+const KEY_AUSENTE = 'A'
+const KEY_PRESENTE = 'L'
 
 var tempo = []
 var escolhas = []
@@ -6,24 +10,83 @@ var nivel = 1
 
 var alternar = document.getElementsByClassName('alternar')[0]
 var lembrar = document.getElementsByClassName('lembrar')[0]
+var botoes = document.getElementsByClassName('botoes')[0]
 
-var aleatorio = alfabeto[Math.floor(Math.random() * alfabeto.length)]
-alternar.innerHTML = aleatorio
+function comecar(){
+    botoes.classList.remove('hidden')
+    alternar.innerHTML = letra_aleatoria()
+    lembrar.innerHTML = letra_aleatoria()+letra_aleatoria()
+    alternar.removeAttribute('onclick')
+    
+    pegaTempo()
+
+    window.addEventListener('keypress', listener)
+}
+
+function finalizar(){
+    console.log(escolhas)
+    console.log(resultado(tempo))
+    window.removeEventListener('keypress', listener)
+
+    alternar.innerHTML = "FIM!"
+    lembrar.innerHTML = ''
+    botoes.classList.add('hidden')
+}
+
+var listener = function sternberg (e) {
+    pegaTempo()
+    var codigo = e.code.slice(3)
+    var acertou = false
+
+    if (codigo == KEY_AUSENTE) { // Escolha do usuario
+        if (!lembrar.innerHTML.includes(alternar.innerHTML)) {
+            acertou = true
+        }
+    } else if(codigo == KEY_PRESENTE){
+        if (lembrar.innerHTML.includes(alternar.innerHTML)) {
+            acertou = true
+        }
+    }
+
+    if (acertou) { //Inserção no vetor de escolhas
+        escolhas.push(true)
+    }else{
+        escolhas.push(false)
+    }
+
+
+    if (escolhas.length%FIM == 0) {
+        finalizar()
+    }else if (escolhas.length%PROX_NIVEL == 0) {
+        nivel = nivel+1
+        proximo_nivel()
+    }
+    
+
+    alternar.innerHTML = letra_aleatoria()
+}
+
+function letra_aleatoria(){
+    return alfabeto[Math.floor(Math.random() * alfabeto.length)]
+}
 
 function pegaTempo() {
     var d = new Date()
     tempo.push(d)
 }
 
-window.addEventListener('keypress', function (e) {
-    var codigo = e.code.slice(3)
-    if (codigo == 'A') {
-        if (lembrar.innerHTML.search(codigo) != -1) {
-            
-        }
-    } else if(codigo == 'L'){
-        
-    }else{
-
+function proximo_nivel(){
+    var frase = ''
+    for (let i = 0; i < nivel; i++) {
+        frase = frase+letra_aleatoria()+letra_aleatoria()
     }
-});
+    lembrar.innerHTML = frase
+}
+
+function resultado(a){
+    var dif = new Array
+    for (var i = 0; i < a.length-1; i++) {
+        dif.push(a[i+1]-a[i])
+    }
+    return dif
+}
