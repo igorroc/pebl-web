cd1ac41222a2c2d62a28d09aa6dd95c178c27889
IgorRoc <igor_roc@hotmail.com.br>
2021-03-27 16:01:55 -0300

    [end]: Finalizado

--- js/sternberg.js
+++ js/sternberg.js
@@ -3,24 +3,44 @@ const PROX_NIVEL = 10
 const FIM = 30
 const KEY_AUSENTE = 'A'
 const KEY_PRESENTE = 'L'
+const FRASE_FINAL = 'Parabéns! Você concluiu o teste.\nAgora você pode retornar ao menu principal clicando em home.'
 
 var tempo = []
 var escolhas = []
 var nivel = 1
+var transicionando = false
 
+var body = document.getElementsByTagName('body')[0]
+var teste = document.getElementsByClassName('teste')[0]
 var alternar = document.getElementsByClassName('alternar')[0]
 var lembrar = document.getElementsByClassName('lembrar')[0]
 var botoes = document.getElementsByClassName('botoes')[0]
 
-function comecar(){
+// INICIO DINAMICO
+lembrar.innerHTML = letra_aleatoria()+letra_aleatoria()
+
+
+async function comecar(){
     botoes.classList.remove('hidden')
-    alternar.innerHTML = letra_aleatoria()
-    lembrar.innerHTML = letra_aleatoria()+letra_aleatoria()
     alternar.removeAttribute('onclick')
-    
+
+    alternar.innerHTML = '3'
+    await sleep(1000)
+    alternar.innerHTML = '2'
+    await sleep(1000)
+    alternar.innerHTML = '1'
+    await sleep(1000)
+    alternar.innerHTML = letra_aleatoria()
+
+    console.log('comecou')
+
     pegaTempo()
 
     window.addEventListener('keypress', listener)
+}
+
+function removeComeco(){
+    window.removeEventListener('keyup', comecar)
 }
 
 function finalizar(){
@@ -28,45 +48,81 @@ function finalizar(){
     console.log(resultado(tempo))
     window.removeEventListener('keypress', listener)
 
-    alternar.innerHTML = "FIM!"
-    lembrar.innerHTML = ''
-    botoes.classList.add('hidden')
+    var div =  document.createElement('div')
+    var p = document.createElement('p')
+    p.innerText = FRASE_FINAL
+    div.classList.add('transicao')
+    div.appendChild(p)
+    body.appendChild(div)
+    
+    teste.classList.add('hidden')
+}
+
+function transicao(texto){
+    var div =  document.createElement('div')
+    var p = document.createElement('p')
+    p.innerText = texto
+    div.classList.add('transicao')
+    div.appendChild(p)
+
+    var teste = document.getElementsByClassName('teste')[0]
+    teste.classList.add('hidden')
+
+    body.appendChild(div)
+}
+
+function removeTransicao(){
+    nivel = nivel+1
+    transicionando = false
+    var trans = document.getElementsByClassName('transicao')[0]
+    trans.remove()
+
+    var testes = document.getElementsByClassName('teste')[0]
+    testes.classList.remove('hidden')
+
+    proximo_nivel()
+    lembrar.classList.remove('hidden')
 }
 
 var listener = function sternberg (e) {
-    pegaTempo()
-    var codigo = e.code.slice(3)
-    var acertou = false
-
-    if (codigo == KEY_AUSENTE) { // Escolha do usuario
-        if (!lembrar.innerHTML.includes(alternar.innerHTML)) {
-            acertou = true
-        }
-    } else if(codigo == KEY_PRESENTE){
-        if (lembrar.innerHTML.includes(alternar.innerHTML)) {
-            acertou = true
-        }
-    }
-
-    if (acertou) { //Inserção no vetor de escolhas
-        escolhas.push(true)
-        lembrar.classList.add('hidden')
+    console.log('clicou')
+    if (transicionando) {
+        removeTransicao()
+        console.log('remove transicao')
     }else{
-        escolhas.push(false)
-        lembrar.classList.remove('hidden')
-    }
-
-
-    if (escolhas.length%FIM == 0) {
-        finalizar()
-    }else if (escolhas.length%PROX_NIVEL == 0) {
-        nivel = nivel+1
-        proximo_nivel()
-        lembrar.classList.remove('hidden')
-    }
+        pegaTempo()
+        var codigo = e.code.slice(3)
+        var acertou = false
     
-
-    alternar.innerHTML = letra_aleatoria()
+        if (codigo == KEY_AUSENTE) { // Escolha do usuario
+            if (!lembrar.innerHTML.includes(alternar.innerHTML)) {
+                acertou = true
+            }
+        } else if(codigo == KEY_PRESENTE){
+            if (lembrar.innerHTML.includes(alternar.innerHTML)) {
+                acertou = true
+            }
+        }
+    
+        if (acertou) { //Inserção no vetor de escolhas
+            escolhas.push(true)
+            lembrar.classList.add('hidden')
+        }else{
+            escolhas.push(false)
+            lembrar.classList.remove('hidden')
+        }
+    
+    
+        if (escolhas.length%FIM == 0) {
+            finalizar()
+        }else if (escolhas.length%PROX_NIVEL == 0) {
+            transicionando = true
+            transicao(`Indo para o nivel ${nivel+1}.\nAperte qualquer tecla para continuar!`)
+        }
+        
+    
+        alternar.innerHTML = letra_aleatoria()
+    }
 }
 
 function letra_aleatoria(){
@@ -92,4 +148,10 @@ function resultado(a){
         dif.push(a[i+1]-a[i])
     }
     return dif
+}
+
+function sleep(ms) {
+    return new Promise(
+      resolve => setTimeout(resolve, ms)
+    );
 }
