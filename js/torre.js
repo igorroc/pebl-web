5df5a5deb173cc96d8f111992237d990b92b57f9
IgorRoc <igor_roc@hotmail.com.br>
2021-05-04 23:32:19 -0300

    [add]: Functioning with max of 3 stacked

--- js/torre.js
+++ js/torre.js
@@ -5,16 +5,29 @@ var slots = [document.getElementById("slot1"),
 var mao = document.getElementById("mao")
 
 for (const slot of slots) {
-    slot.addEventListener("click", () => {
+    slot.addEventListener("click", async () => {
         if(mao.children.length > 0){
+            // await animacaoIn(mao.children[0], 300)
             colocaPosicao(mao.children[0], slot)
             slot.append(mao.children[0])
+            // await animacaoOut(slot.children[0], 300)
         }else{
+            // await animacaoIn(slot.children[slot.children.length-1], 300)
             mao.append(slot.children[slot.children.length-1])
             limpaPosicao(mao.children[0])
+            // await animacaoOut(mao.children[0], 300)
         }
     })
 }
+
+var i = 0
+for (const slot of slots) {
+    for (const disco of slot.getElementsByClassName("disco")) {
+        disco.innerHTML = i++
+    }
+}
+
+// FUNCTIONS
 
 function limpaPosicao(el) {
     el.classList.remove("row-1")
@@ -24,4 +37,24 @@ function limpaPosicao(el) {
 
 function colocaPosicao(el, slot){
     el.classList.add(`row-${3-slot.children.length}`)
+}
+
+async function animacaoIn(el, time){
+    console.log(el)
+    el.classList.add("diminuir")
+    await sleep(time)
+    el.classList.add("espera")
+    el.classList.remove("diminuir")
+}
+
+async function animacaoOut(el, time){
+    console.log(el)
+    el.classList.add("aumentar")
+    el.classList.remove("espera")
+    await sleep(time)
+    el.classList.remove("aumentar")
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
