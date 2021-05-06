e47015ec5920e76812e77605a6575b887665a07b
IgorRoc <igor_roc@hotmail.com.br>
2021-05-06 09:24:18 -0300

    [add]: User can't scroll while the page is loading

--- js/loader.js
+++ js/loader.js
@@ -1,6 +1,9 @@
 var loader_wrapper = document.getElementById("loader-wrapper")
+var body = document.body
+
 
 window.addEventListener("load", () => {
     loader_wrapper.classList.add("sumir")
     loader_wrapper.children[0].classList.add("pausado")
+    body.classList.remove("stop-scrolling")
 })
