cc567d7dbb89bc97ce253dec75b481a5f01e6ee0
IgorRoc <igor_roc@hotmail.com.br>
2021-05-06 07:38:06 -0300

    [add]: Loader css in a separated file

---
+++ js/loader.js
@@ -0,0 +1,6 @@
+var loader_wrapper = document.getElementById("loader-wrapper")
+
+window.addEventListener("load", () => {
+    loader_wrapper.classList.add("sumir")
+    loader_wrapper.children[0].classList.add("pausado")
+})
