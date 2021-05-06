9c56c52602b65564f5d861d898d7546a05f2fc99
IgorRoc <igor_roc@hotmail.com.br>
2021-05-06 19:15:11 -0300

    [add]: When loading, only displays the loader

--- js/loader.js
+++ js/loader.js
@@ -1,8 +1,10 @@
 var loader_wrapper = document.getElementById("loader-wrapper")
 var body = document.body
+var everything = document.getElementById("everything")
 
 
 window.addEventListener("load", () => {
+    everything.classList.remove("displaynone")
     loader_wrapper.classList.add("sumir")
     loader_wrapper.children[0].classList.add("pausado")
     body.classList.remove("stop-scrolling")
