4f7662b0e5c9fff74a7ee6e1cf5bb875aa3798e6
IgorRoc <igor_roc@hotmail.com.br>
2021-05-07 20:56:04 -0300

    [new]: Main functions to a different file

---
+++ js/functions.js
@@ -0,0 +1,22 @@
+const ALFABETO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+function getTime(local) {
+    var t = new Date()
+    local.push(t)
+}
+
+function resultado(a) {
+    var dif = new Array
+    for (var i = 0; i < a.length - 1; i++) {
+        dif.push(a[i + 1] - a[i])
+    }
+    return dif
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function letra_aleatoria() {
+    return ALFABETO[Math.floor(Math.random() * ALFABETO.length)]
+}
