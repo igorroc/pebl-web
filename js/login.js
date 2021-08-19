a005722521bd93ecbfb166d2322c7cc24bcef4c7
IgorRoc <igor_roc@hotmail.com.br>
2021-08-18 23:59:05 -0300

    [new]: Refactoring login page

--- js/login.js
+++ js/login.js
@@ -3,3 +3,8 @@ var loader = document.getElementById("loader")
 loader.addEventListener("click", () => {
 	loader.classList.toggle("pausado")
 })
+
+function switchForm() {
+	let form = document.querySelector("#wrapper-inner-form")
+	form.classList.toggle("show-sign-up")
+}
