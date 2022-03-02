871a21720932386f28c9649ab336f9b5bb5ec333
deboragama <debora.miranda@cepedi.org.br>
2022-03-02 15:56:43 -0300

    [add] Criação das rotas para armazenar os pacientes (Tarefa #34670014)

--- src/modules/patients/infra/http/routes/patient.routes.ts
+++ src/modules/patients/infra/http/routes/patient.routes.ts
@@ -1,11 +1,14 @@
-import { Router } from 'express';
-import { container } from 'tsyringe';
-import PatientController from '../controllers/PatientController';
+import { Router } from "express";
+import { container } from "tsyringe";
+import PatientController from "../controllers/PatientController";
 
 const patientRouter = Router();
 const patientController = container.resolve(PatientController);
 
-patientRouter.post('/create', patientController.create);
-patientRouter.post('/cpf', patientController.searchCpf)
-
+patientRouter.get("/all", patientController.listAll);
+patientRouter.post("/create", patientController.create);
+patientRouter.post("/cpf", patientController.searchCpf);
+patientRouter.get("/:id", patientController.searchId);
+patientRouter.put("/update/:id", patientController.update);
+patientRouter.delete("/delete/:id", patientController.delete);
 export default patientRouter;
--- src/modules/patients/services/PatientService.ts
+++ src/modules/patients/services/PatientService.ts
@@ -1,29 +1,49 @@
-import { injectable, inject } from 'tsyringe';
-import ICreatePatientDtos from '../dtos/ICreatePatientDtos';
-import Patient from '../infra/typeorm/entities/Patients';
-import IPatientRepository from '../interfaces/IPatientRepository';
-import { IPatientService } from '../interfaces/IPatientService';
+import { injectable, inject } from "tsyringe";
+import { DeleteResult, UpdateResult } from "typeorm";
+import ICreatePatientDtos from "../dtos/ICreatePatientDtos";
+import Patient from "../infra/typeorm/entities/Patients";
+import IPatientRepository from "../interfaces/IPatientRepository";
+import { IPatientService } from "../interfaces/IPatientService";
 
 @injectable()
 class PatientService implements IPatientService {
+  constructor(
+    @inject("PatientRepository")
+    private patientRepository: IPatientRepository
+  ) {}
 
-    constructor(
-        @inject('PatientRepository')
-        private patientRepository: IPatientRepository,
-    ) { }
+  async searchCpf(cpf: string): Promise<Patient> {
+    const result = await this.patientRepository.findByCPF(cpf);
+    return result;
+  }
 
-    async searchCpf(cpf: string): Promise<Patient> {
-        const result = await this.patientRepository.findByCPF(cpf) 
-        return result
+  async searchId(id: string): Promise<Patient> {
+    const result = await this.patientRepository.findById(id);
+    return result;
+  }
+
+  async listAll(): Promise<Patient[]> {
+    const result = await this.patientRepository.listAll();
+    return result;
+  }
+
+  async update(id: string, patient: Patient): Promise<UpdateResult> {
+    const result = await this.patientRepository.update(id, patient);
+    return result;
+  }
+
+  async delete(id: string): Promise<DeleteResult> {
+    const result = await this.patientRepository.delete(id);
+    return result;
+  }
+
+  async execute(data: ICreatePatientDtos): Promise<Patient> {
+    const result = await this.patientRepository.findByCPF(data.cpf);
+    if (!result) {
+      return await this.patientRepository.create(data);
     }
-
-    async execute(data: ICreatePatientDtos): Promise<Patient> {
-        const result = await this.patientRepository.findByCPF(data.cpf) 
-        if(!result){
-            return await this.patientRepository.create(data)
-        }
-        return result
-    }
+    return result;
+  }
 }
 
 export default PatientService;
