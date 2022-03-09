636989e14d8ff3bd686e72f04190bde1610cb43b
deboragama <debora.miranda@cepedi.org.br>
2022-03-09 17:13:42 -0300

    [fix] Alterar criação do cookie feita no backend para o frontend (Tarefa #34670007)

--- js/loggedUser.js
+++ js/loggedUser.js
@@ -6,16 +6,6 @@ loader.addEventListener("click", () => {
 
 var userTitle = document.getElementById("userTitle")
 
-function axiosConfig(){
-	const Api = axios.create({
-		baseURL: "http://127.0.0.1:3333",
-		credentials: 'include',
-		withCredentials:true,
-		headers:{"Content-Type": "application/json"},
-	});
-	return Api
-}
-
 async function getUser(){
 	const Api = axiosConfig()
 	let dataPost
--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -14,6 +14,8 @@ export default class SessionsController {
             password,
         });
         delete user.password;
+        
+        response.setHeader("Set-Cookie", [`token=${token};Path=/;SameSite=None;Secure;`]);
 
         return response.json({ user, token });  
     } 
